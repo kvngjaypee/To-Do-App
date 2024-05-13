12c3b8a30f88edd87eab6e8df2f904acc77cec7b
kvngjaypee <johnpaulanyanwu17@gmail.com>
2024-05-13 14:59:39 +0100

    added few lines of code

--- to-do.js
+++ to-do.js
@@ -13,28 +13,69 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+const addOrUpdateTask = () => {
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
+
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+
+  updateTaskContainer();
+  reset();
+};
+
+const updateTaskContainer = () => {
+  tasksContainer.innerHTML = "";
+  taskData.forEach(({ id, title, date, description }) => {
+    tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button type="button" class="btn">Edit</button>
+          <button type="button" class="btn">Delete</button> 
+        </div>
+      `;
+  });
+};
+
+const reset = () => {
+  titleInput.value = "";
+  dateInput.value = "";
+  descriptionInput.value = "";
+  taskForm.classList.toggle("hidden");
+  currentTask = {};
+};
+
 openTaskFormBtn.addEventListener("click", () =>
-    taskForm.classList.toggle("hidden")
+  taskForm.classList.toggle("hidden")
 );
 
 closeTaskFormBtn.addEventListener("click", () => {
+  const formInputsContainValues =
+    titleInput.value || dateInput.value || descriptionInput.value;
+  if (formInputsContainValues) {
     confirmCloseDialog.showModal();
+  } else {
+    reset();
+  }
 });
 
 cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
 
 discardBtn.addEventListener("click", () => {
-    confirmCloseDialog.close();
-    taskForm.classList.toggle("hidden");
+  confirmCloseDialog.close();
+  reset();
 });
 
 taskForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-
-    const taskObj = {
-        id: titleInput.value.toLowerCase().split(" "),
-    };
-    console.log(taskObj);
+  addOrUpdateTask();
 });
